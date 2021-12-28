8503aa0589e4a1987ea76ff79f0f1adde7b2e617
frippertronics <frippertron1cs@protonmail.com>
2021-12-28 17:46:34 +0100

    Made result printing constant after test

--- include/kabeltester/tester.h
+++ include/kabeltester/tester.h
@@ -24,5 +24,6 @@ void TESTER_Setup(void);
 
 // Note: This function expects an input array with a length >= 37
 bool TESTER_TestLk37(TEST_RESULTS_T* test_results);
+void TESTER_DisplayTestResults(bool cableHasErrors, TEST_RESULTS_T* testResultArray);
 
 #endif //TESTER_H
